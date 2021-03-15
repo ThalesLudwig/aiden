2067ea277eb7cc18c1d99f6c79f9edb3161be862
thalesludwig <thales.ludwig@gmail.com>
2021-03-15 11:39:52 -0300

    Fixed nullable lang in Finder

--- src/orchestrator/index.js
+++ src/orchestrator/index.js
@@ -2,11 +2,12 @@ const parser = require("../parser");
 const persistToContext = require("../context");
 const dealer = require("../dealer");
 const Finder = require("../modules/finder");
+const LANG_CONSTANTS = require("../constants/lang");
 
 const orchestrator = (input) => {
   const content = parser(input);
   if (content.intention.length === 0) {
-    const nothingFound = Finder({ lang: input.lang });
+    const nothingFound = Finder({ lang: input.lang || LANG_CONSTANTS.EN_US });
     return nothingFound;
   }
   persistToContext(content);
